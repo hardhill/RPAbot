17ff3dcc666724113669bd65b75fc879f3aafd3f
W.Bielecki <hardhill@mail.ru>
2020-08-03 15:30:06 +0800

    v4

--- OtherBot/Program.cs
+++ OtherBot/Program.cs
@@ -13,9 +13,10 @@ namespace OtherBot
     {
         static async Task Main(string[] args)
         {
-            WebProxy proxyObject = new WebProxy("http://10.3.239.2:3128/",false);
+            WebProxy proxyObject = new WebProxy("http://10.3.239.2:3128/",true);
             ServicePointManager.Expect100Continue = true;
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+           // ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
             TelegramBot bot = new TelegramBot("1333442954:AAEA2Zn5jkvRc3ag6Cv8qdJyX2Bo8G3Fb68");
             var req = new GetMe();
             bot.WebProxy = proxyObject;
--- SimpleBot/Program.cs
+++ SimpleBot/Program.cs
@@ -15,6 +15,7 @@ namespace SimpleBot
             TelegramBotClient bot = new TelegramBotClient("1333442954:AAEA2Zn5jkvRc3ag6Cv8qdJyX2Bo8G3Fb68");
             ServicePointManager.Expect100Continue = true;
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls|SecurityProtocolType.Ssl3;
+            ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
             var me = await bot.GetMeAsync();
             Console.WriteLine(me.Username);
             Console.ReadLine();
